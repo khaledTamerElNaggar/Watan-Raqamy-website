3c3ceee32a651b8bb2b61c9c79cc223e3a30b343
Khaled Tamer <Khaledfunnew@gmail.com>
2024-12-11 13:26:17 +0200

    Update script.js

--- LoginPage/script.js
+++ LoginPage/script.js
@@ -31,17 +31,3 @@ function registerUser() {
       document.getElementById('linkedin').value = '';
   }
 }
-
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxDSaMnag6pKYHCf8CIY6hIRMltmP8iuB0_0xwopn--wukwe_Z08IXqiP-y2PcCecJMpQ/exec'
-
-const form = document.forms['contact-form']
-
-form.addEventListener('submit', e => {
-  
-  e.preventDefault()
-  
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! Form is submitted" ))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
-})
