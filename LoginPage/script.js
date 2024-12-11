7a288901ec2a692e28d34c98545fa2005b15c0aa
Khaled Tamer <Khaledfunnew@gmail.com>
2024-12-11 12:14:06 +0200

    Update script.js

--- LoginPage/script.js
+++ LoginPage/script.js
@@ -45,13 +45,3 @@ form.addEventListener('submit', e => {
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
 })
-
-function registerUser1('submit', e => {
-  
-  e.preventDefault()
-  
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! Form is submitted" ))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
-})
